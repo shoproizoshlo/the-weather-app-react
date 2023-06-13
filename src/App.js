ac9ca2095ba23ce9b120c6d1dde784d49b4d994d
shoproizoshlo <iamsusunny@gmail.com>
2023-06-13 15:47:37 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       Weather App
+      <div>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+      </div>
       <footer>
         This App crated by Sue Brechko and is open-soursed on Github
       </footer>
