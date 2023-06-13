8a1ef24665a55ea999d92d56b053d5cdc1c8d088
shoproizoshlo <iamsusunny@gmail.com>
2023-06-13 13:02:29 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This App crated by Sue Brechko and is open-soursed on Github
+      </footer>
+    </div>
+  );
 }
