1b4f4e5fbeaba0523030a4a78b69c436901fd4a7
shoproizoshlo <iamsusunny@gmail.com>
2023-06-15 11:23:15 +0200

    fixed format date

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -1,16 +1,5 @@
-import React from "react";
-
 export default function FormatDate(props) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[props.date.getDay()];
+  let day = props.date.getDay();
   let hour = props.date.getHours();
   let minute = props.date.getMinutes();
   if (hour < 10) {
