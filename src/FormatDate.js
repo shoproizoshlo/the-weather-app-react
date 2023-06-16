a6c584f910a99f1e89ede96111f3d03dac4d651f
shoproizoshlo <iamsusunny@gmail.com>
2023-06-16 09:49:13 +0200

    Update FormatDate.js

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -14,10 +14,10 @@ export default function FormatDate(props) {
   let hours = props.date.getHours();
   let minutes = props.date.getMinutes();
   if (hours < 10) {
-    return `0${hour}`;
+    return `0${hours}`;
   }
   if (minutes < 10) {
-    return `0${minute}`;
+    return `0${minutes}`;
   }
   return `${day} ${hours}:${minutes}`;
 }
